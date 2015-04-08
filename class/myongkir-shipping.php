3f88d74d9033e08f8eba014d7b8c97549e332bee
eezhal92 <arkinthesky.69@gmail.com>
2015-04-08 20:42:47 +0700

    remove currency function

--- class/myongkir-shipping.php
+++ class/myongkir-shipping.php
@@ -43,23 +43,6 @@ class MyOngkir_Shipping {
 	public function set_api_key($api_key) {
 		$this->api_key = $api_key;
 	}
-	
-	// get currency from indonesian rupiah to desired currency
-	public function get_currency($target_currency, $amount = 1) {
-		$req = new Request(array(
-	 			// 'server' => 'http://www.freecurrencyconverterapi.com/api/v3/'
-				'server' => 'http://www.getexchangerates.com/api/convert/'
-	 	));
-		
-		$uri = $amount .'/IDR/'. $target_currency;
-		
-		$result = $req->get($uri, array());
-		
-		// $result = object_to_array( $result );
-		var_dump($result->response);
-		die();
-		
-	}
 
 	/**
 	* get_cities function.
